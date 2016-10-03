3ea2abb361058cbf44637bf2d5f8ab866aed7314
Arun Y <arunwizz@gmail.com>
2016-10-03 22:54:48 +0530

    1. moved resources outside configuration 2. Added rowset as system level dependency 3. modified run script to order jars loading 4. removed unwanted jars.

--- src/main/scripts/runprofiler.bat
+++ src/main/scripts/runprofiler.bat
@@ -2,4 +2,4 @@
 @echo Need java 1.8.x or above to run
 @echo: 
 @java -version
-java -Xmx4096M -Xms4096M -classpath osdq-desktop-${project.version}.jar;lib\rowset.jar;lib\* org.arrah.gui.swing.Profiler %1
+java -Xmx4096M -Xms4096M -classpath osdq-desktop-${project.version}.jar;lib/rowset.jar;lib/jdbcjar/*;lib/hivejar/*;lib/hive2jar/*;lib/* org.arrah.gui.swing.Profiler $1
